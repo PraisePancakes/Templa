218600913c2be1f62d5c2af94a0d514205b26319
PraisePancakes <100659653+PraisePancakes@users.noreply.github.com>
2025-05-30 15:06:42 -0400

    convert type_identity

--- src/templa.hpp
+++ src/templa.hpp
@@ -36,27 +36,23 @@ namespace templa
     namespace convert
     {
         template <typename... Ts>
-        struct convert_to_variant
+        struct convert_to_variant : std::type_identity<std::variant<Ts...>>
         {
-            using type = std::variant<Ts...>;
         };
 
         template <template <typename...> class From, typename... Ts>
-        struct convert_to_variant<From<Ts...>>
+        struct convert_to_variant<From<Ts...>> : std::type_identity<std::variant<Ts...>>
         {
-            using type = std::variant<Ts...>;
         };
 
         template <typename... Ts>
-        struct convert_to_tuple
+        struct convert_to_tuple : std::type_identity<std::tuple<Ts...>>
         {
-            using type = std::tuple<Ts...>;
         };
 
         template <template <typename...> class From, typename... Ts>
-        struct convert_to_tuple<From<Ts...>>
+        struct convert_to_tuple<From<Ts...>> : std::type_identity<std::tuple<Ts...>>
         {
-            using type = std::tuple<Ts...>;
         };
     }
 
