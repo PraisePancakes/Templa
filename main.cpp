10c2cfbdc55f29c3be6824fd8edfacea15f2e9cd
PraisePancakes <100659653+PraisePancakes@users.noreply.github.com>
2025-05-30 14:10:58 -0400

    is_same_signature

--- main.cpp
+++ main.cpp
@@ -18,6 +18,9 @@ int main(int argc, char **argv)
     std::function<int(int, int)> g = [](int x, int y)
     { return x + y; };
 
+    std::function<char(int, char)> l = [](int x, char y)
+    { return 'a'; };
+
     using t = return_type_t<decltype(f)>;
 
     constexpr auto lam1 = [](int x, int y) -> int
@@ -33,6 +36,8 @@ int main(int argc, char **argv)
     static_assert(std::is_same_v<t, int>);
     static_assert(!is_same_return_type_v<decltype(test_f), decltype(f)>);
     static_assert(is_same_return_type_callable_v<decltype(lam1), decltype(lam2), int, int>);
+    static_assert(is_same_signature<decltype(f), decltype(g), int, int>::value);
+    static_assert(!is_same_signature<decltype(f), decltype(l), int, int>::value);
 
     return 0;
 }
