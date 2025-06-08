8ccdbcb4d6940a4ccfdd56891a0fc54e5926b60c
PraisePancakes <100659653+PraisePancakes@users.noreply.github.com>
2025-06-08 12:31:54 -0400

    static_for

--- main.cpp
+++ main.cpp
@@ -6,5 +6,24 @@
 int main(int argc, char **argv)
 {
   templa::test::test_min();
+
+  constexpr auto filled_array = []()
+  {
+    std::array<int, 5> arr{};
+    constexpr int x = 1;
+    constexpr int y = 2;
+
+    templa::algorithms::static_for<5>([&]<std::size_t... I>(std::array<int, 5> &a, int x, int y) constexpr
+                                      { ((a[I] = static_cast<int>(x * y)), ...); }, arr, x, y);
+
+    return arr;
+  }();
+
+  static_assert(filled_array[0] == 2);
+  static_assert(filled_array[1] == 2);
+  static_assert(filled_array[2] == 2);
+  static_assert(filled_array[3] == 2);
+  static_assert(filled_array[4] == 2);
+
   return 0;
 }
--- src/templa-src/algorithms.hpp
+++ src/templa-src/algorithms.hpp
@@ -262,6 +262,15 @@ namespace templa
             };
         };
 
+        template <std::size_t N, typename F, typename... Args>
+        [[nodiscard]] constexpr auto static_for(F &&f, Args &&...args)
+        {
+            return []<std::size_t... I>(std::index_sequence<I...>, F &&f, Args &&...args) constexpr
+            {
+                std::forward<F>(f).template operator()<I...>(std::forward<Args>(args)...);
+            }(std::make_index_sequence<N>{}, std::forward<F>(f), std::forward<Args>(args)...);
+        };
+
         template <typename Callable, typename... Ts>
         constexpr static void apply_to_tuple_cat(Callable &&c, Ts &&...tuples)
         {
